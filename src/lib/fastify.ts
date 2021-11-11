eb27683181fd9029b1ad2a1468bf6b974c1cd8bf
Thomas VEILLARD <thomasveillard@live.fr>
2021-11-11 18:07:00 +0100

    feat: error handler (drop 500 message, support 422)

--- src/lib/fastify.ts
+++ src/lib/fastify.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import cookie, { FastifyCookieOptions } from 'fastify-cookie'
+import { UnauthorizedError } from '../policies/policy'
 import { sessionRoutes } from '../routes/sessions'
 import { userRoutes } from '../routes/users'
 import { COOKIE_SECRET, FASTIFY_LOGGING } from './dotenv'
@@ -11,3 +12,15 @@ export const server = fastify({ logger: FASTIFY_LOGGING })
   .addHook('preHandler', loadSession)
   .register(sessionRoutes, { prefix: '/sessions' })
   .register(userRoutes, { prefix: '/users' })
+  .setErrorHandler((error, request, reply) => {
+    // based on https://github.com/fastify/fastify/blob/1e94070992d911a81a26597c25f2d35ae65f3d91/fastify.js#L74
+    if (error instanceof UnauthorizedError) {
+      void reply.status(422).send(error)
+    } else if (reply.statusCode < 500) {
+      reply.log.info({ res: reply, err: error }, error?.message)
+      void reply.send(error)
+    } else {
+      reply.log.error({ req: request, res: reply, err: error }, error?.message)
+      void reply.send(new Error('Internal Server Error, message dropped'))
+    }
+  })
--- src/specs/routes/users.spec.ts
+++ src/specs/routes/users.spec.ts
@@ -3,6 +3,7 @@ import { createSessionFixture } from '../fixtures/sessions-fixtures'
 import { sign } from 'cookie-signature'
 import { COOKIE_NAME, COOKIE_SECRET } from '../../lib/dotenv'
 import { expect } from 'chai'
+import { createUserFixture } from '../fixtures/users-fixtures'
 
 describe('/users', function () {
   describe('#show', function () {
@@ -12,6 +13,15 @@ describe('/users', function () {
       const response = await server.inject({ method: 'GET', url: '/users/me', cookies })
       expect(response.statusCode).to.eq(200)
       expect(response.json()).to.haveOwnProperty('id').equal(session.user.id)
+    })
+
+    it('should reject if current user is not authorized', async function () {
+      const session = await createSessionFixture()
+      const anotherUser = await createUserFixture()
+      const cookies = { [COOKIE_NAME]: sign(session.id, COOKIE_SECRET) }
+      const response = await server.inject({ method: 'GET', url: `/users/${anotherUser.id}`, cookies })
+      expect(response.statusCode).to.eq(422)
+      expect(response.json()).to.haveOwnProperty('message').equal('You are not allowed to perform this action')
     })
   })
 })
